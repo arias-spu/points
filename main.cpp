b2b08bac65853425185e11fc9de028c2e73cd81b
Carlos R Arias <arias@spu.edu>
2018-02-20 16:07:50 -0800

    Some other point

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-	Point t(1,5), r(0,-1);
+	Point t(1,5), r(0,-1), s(0,0);
 	cout << t.toString() << endl;
 	cout << r.toString() << endl;
 	cout << t.distance(r) << endl;
+	cout << s.distance(r) << endl;
 	return 0;
 }
